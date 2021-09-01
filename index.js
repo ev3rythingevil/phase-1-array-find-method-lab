c66f9521698b3d0159c65d5c31ed5a6da2a1b4a7
Hathor Hamilton <koshknot@gmail.com>
2021-09-01 18:36:04 -0400

    commit tree 6fac514a0ab9026c1593e76600387ff71326a0ae

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function superbowlWin(record){
+   // iterate through object keys, looking for W's
+   // return the key that is associated with the W
+   // if no W then return undefined
+   // the arguemnt will be the array, which is full of objects. 
+   // we need to iterate through the array, and each object
+   // .find iterates, and returns when the callback function's parameter is met
+    const foundWin = record.find((element)=>{
+          if (element.result === 'W'){
+              return true;
+          }         
+      })
+    if (foundWin != undefined){
+        return foundWin.year;
+    }
+    else{
+        return undefined;
+    }   
+}
+      
+
 
